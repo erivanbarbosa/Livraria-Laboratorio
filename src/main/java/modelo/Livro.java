ba54d7f98b3c9d1f35a85182d561284004268a23
douglas <glock_bocaum@hotmail.com>
2018-04-14 17:06:33 -0300

    adiciona modelo livro

---
+++ src/main/java/modelo/Livro.java
@@ -0,0 +1,16 @@
+package modelo;
+
+
+
+public class Livro {
+	private int codLivro;
+	private String titulo;
+	private String autor;
+	private double preco;
+	private String imagem;
+	private String descricao;
+	
+	
+
+}
+
