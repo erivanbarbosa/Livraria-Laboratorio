0de707020e70ff9c6efdc862b88c7cdbdc130681
douglas <glock_bocaum@hotmail.com>
2018-04-28 14:55:34 -0300

    Commit DouglasLPO

--- src/main/java/modelo/Livro.java
+++ src/main/java/modelo/Livro.java
@@ -1,7 +1,5 @@
 package modelo;
 
-
-
 public class Livro {
 	private int codLivro;
 	private String titulo;
@@ -9,8 +7,53 @@ public class Livro {
 	private double preco;
 	private String imagem;
 	private String descricao;
-	
-	
+
+	public int getCodLivro() {
+		return codLivro;
+	}
+
+	public void setCodLivro(int codLivro) {
+		this.codLivro = codLivro;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public String getImagem() {
+		return imagem;
+	}
+
+	public void setImagem(String imagem) {
+		this.imagem = imagem;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 
 }
-
