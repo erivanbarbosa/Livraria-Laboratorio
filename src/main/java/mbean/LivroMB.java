d3f9aef0f036082d6a9f5d3e8cb7c658e74ec5c7
JAVA TER E QUI <JAVA TER E QUI@GYNLAB-STALL08>
2018-05-05 14:36:30 -0300

    alteracao LivroMB

--- src/main/java/mbean/LivroMB.java
+++ src/main/java/mbean/LivroMB.java
@@ -14,9 +14,61 @@ public class LivroMB {
 	private int idLivro;
 	private LivroServico servico;
 	private String titulo;
+	private String autor;
+	private double preco;
+	private String imagem;
+	private String descricao;
 	
 	
 	
+
+	public Livro getLivro() {
+		return livro;
+	}
+
+	public void setLivro(Livro livro) {
+		this.livro = livro;
+	}
+
+	public LivroServico getServico() {
+		return servico;
+	}
+
+	public void setServico(LivroServico servico) {
+		this.servico = servico;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public String getImagem() {
+		return imagem;
+	}
+
+	public void setImagem(String imagem) {
+		this.imagem = imagem;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 
 	public String getTitulo() {
 		return titulo;
